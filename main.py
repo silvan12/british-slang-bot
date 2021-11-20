bc184bd018366cd0520963f768e406d6053b25cb
Silvan <72661914+silvan12@users.noreply.github.com>
2021-11-20 13:24:25 +0100

    final commit

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ import json
 from discord_slash import SlashCommand
 
 client = discord.Client(intents=discord.Intents.all(),
-                        activity=discord.Activity(type=discord.ActivityType.watching, name="you"))
+                        activity=discord.Activity(type=discord.ActivityType.listening, name="UK DRILL"))
 slash = SlashCommand(client, sync_commands=True)
 
-guild_ids = [853612274527109150]
+# Enter your guild ID here
+guild_ids = []
+
+# Enter your bot token here
+token = ""
 
 with open("Data/slang.json") as file:
     slang = json.load(file)
+    
+@slash.slash(name="definition",
+             description="Test your British slang knowledge",
+             guild_ids=guild_ids)
+async def definition(ctx):
+    word = random.choice(list(slang))
+    _definition = slang[word]
+    await ctx.send(f"\"{word}\"\n{_definition} ")
+    
+client.run(token)
